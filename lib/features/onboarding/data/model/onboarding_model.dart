ff86cc12742e17d4997cc58f1eb389103de24b33
eyad-gomaa <99418096+eyad-gomaa@users.noreply.github.com>
2023-12-02 05:40:09 +0200

    add onboarding

---
+++ lib/features/onboarding/data/model/onboarding_model.dart
@@ -0,0 +1,8 @@
+class OnboardingModel{
+  final String title;
+  final String imgUrl;
+  final String about;
+
+  OnboardingModel({required this.title, required this.imgUrl, required this.about});
+
+}
--- lib/features/onboarding/presentation/view/onboarding_view.dart
+++ lib/features/onboarding/presentation/view/onboarding_view.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scooter_app/core/resources/color_manager.dart';
+import 'package:scooter_app/core/shared/constants.dart';
+import 'package:scooter_app/features/onboarding/data/model/onboarding_model.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -7,28 +9,98 @@ class OnBoardingView extends StatefulWidget {
   @override
   State<OnBoardingView> createState() => _OnBoardingViewState();
 }
-final List<Widget> _pages =[];
+
+PageController _controller = PageController();
+int _currentIndex = 0;
+final List<OnboardingModel> _onboardingModels = [
+  OnboardingModel(
+      title: onboarding1Title,
+      imgUrl: "assets/images/Delivery-amico.png",
+      about: onboardingAbout),
+  OnboardingModel(
+      title: onboarding2Title,
+      imgUrl: "assets/images/Instruction_manual.png",
+      about: onboardingAbout),
+  OnboardingModel(
+      title: onboarding3Title,
+      imgUrl: "assets/images/mechanic.png",
+      about: onboardingAbout)
+];
+final List<Widget> _pages = [
+  Page(
+    model: _onboardingModels[0],
+  ),
+  Page(
+    model: _onboardingModels[1],
+  ),
+  Page(
+    model: _onboardingModels[2],
+  ),
+];
+
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        children: [],
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: PageView(
+            controller: _controller,
+            physics: const NeverScrollableScrollPhysics(),
+            children: _pages,
+          ),
+        ),
       ),
     );
-
   }
 }
 
 class Page extends StatelessWidget {
-  const Page({Key? key}) : super(key: key);
+  const Page({Key? key, required this.model}) : super(key: key);
+  final OnboardingModel model;
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text("دليل الصيانه الشامل"),
-        Image.asset("name"),
+        Text(model.title,style: const TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+        Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width/1.9,
+                child: AspectRatio(
+                  aspectRatio: 1/1,
+                    child: Image.asset(model.imgUrl))
+            ),
+            Text(model.about,style: const TextStyle(color: Colors.grey),textAlign: TextAlign.center,),
+          ],
+        ),
+        GestureDetector(
+          onTap: (){
+            if(_currentIndex != 2){
+              _currentIndex += 1;
+              _controller.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+            }else{
+
+            }
+
+          },
+          child: Container(
+            width: MediaQuery.sizeOf(context).width / 2,
+            height: 52,
+            decoration: BoxDecoration(
+                color: ColorManager.redColor,
+                borderRadius: BorderRadius.circular(15)),
+            child: const Center(
+              child: Text(
+                "التالي",
+                style: TextStyle(color: Colors.white,),
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
